e4917c92c7356cbdae6dc6e8626d643e601120c4
Christian Chaux <xwilarg@protonmail.com>
2023-05-07 00:44:28 +0200

    Add fish & hook controller

---
+++ Assets/Scripts/Fishing/FishController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Sketch.Fishing
+{
+    public class FishController : MonoBehaviour
+    {
+        private Rigidbody2D _rb;
+
+        private Vector2 _aimPosition;
+
+        private HookController _target;
+
+        private float _attackTimer;
+        private float AttackTimerRef => Random.Range(1f, 2f);
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+            _rb.velocity = transform.right * .4f;
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.CompareTag("FISHING_Float"))
+            {
+                _rb.velocity = Vector2.zero;
+                var dir = collision.transform.position - transform.position;
+                var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+                transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+                _aimPosition = transform.position;
+
+                _target = collision.GetComponent<HookController>();
+                _attackTimer = AttackTimerRef;
+            }
+        }
+
+        private IEnumerator BitAndWait()
+        {
+            yield return new WaitForSeconds(2f);
+            _target.Hooked = null;
+            _target = null;
+            _rb.velocity = transform.right * .6f;
+        }
+
+        private void Update()
+        {
+            if (_target != null)
+            {
+                if (_target.Hooked != null && _target.Hooked != this)
+                {
+                    _target = null;
+                    _rb.velocity = transform.right * .6f;
+                }
+                else if (_attackTimer > 0f)
+                {
+                    _attackTimer -= Time.deltaTime;
+                    if (_attackTimer <= 0f)
+                    {
+                        if (_target.TakeDamage())
+                        {
+                            _target.Hooked = this;
+                            StartCoroutine(BitAndWait());
+                        }
+                        else
+                        {
+                            _attackTimer = AttackTimerRef;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Fishing/HookController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Sketch.Fishing
+{
+    public class HookController : MonoBehaviour
+    {
+        private int _hp;
+        private SpriteRenderer _sr;
+
+        private FishController _hooked { set; get; }
+        public FishController Hooked
+        {
+            set
+            {
+                _hooked = value;
+                if (value == null)
+                {
+                    _sr.color = Color.white;
+                }
+            }
+            get => _hooked;
+        }
+
+        private void Awake()
+        {
+            _hp = Random.Range(2, 5);
+            _sr = GetComponent<SpriteRenderer>();
+        }
+
+        public bool TakeDamage()
+        {
+            if (_hp == 0) return false;
+            _hp--;
+            StartCoroutine(DamageEffect());
+            return _hp == 0;
+        }
+
+        private IEnumerator DamageEffect()
+        {
+            yield return new WaitForSeconds(.1f);
+            _sr.color = new(1f, 1f, 1f, .5f);
+            yield return new WaitForSeconds(.1f);
+            if (_hp > 0)
+            {
+                _sr.color = Color.white;
+            }
+        }
+    }
+}
