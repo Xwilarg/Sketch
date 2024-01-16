20db9f9e36d485193e48e41aaf77863eef94790b
Xwilarg <xwilarg@protonmail.com>
2024-01-16 19:57:47 +0100

    Add achievement recap

---
+++ Assets/Scripts/Achievement/AchievementDisplay.cs
@@ -0,0 +1,30 @@
+﻿using Sketch.Persistency;
+using TMPro;
+using UnityEngine;
+
+namespace Sketch.Achievement
+{
+    public class AchievementDisplay : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform _container;
+
+        [SerializeField]
+        private GameObject _prefab;
+
+        private void Start()
+        {
+            foreach (var ach in AchievementManager.Instance.Achievements)
+            {
+                var unlocked = PersistencyManager.Instance.SaveData.IsUnlocked(ach.Key);
+
+                var obj = Instantiate(_prefab, _container);
+
+                var texts = obj.GetComponentsInChildren<TMP_Text>();
+
+                texts[0].text = unlocked ? ach.Value.Name : "???";
+                texts[1].text = unlocked ? ach.Value.Description : ach.Value.Hint;
+            }
+        }
+    }
+}
