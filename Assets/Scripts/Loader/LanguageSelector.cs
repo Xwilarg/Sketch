5fb5e1c66952ce4987713f6db903e4c6919430bb
Xwilarg <xwilarg@protonmail.com>
2025-12-09 00:06:43 +0100

    Fix invalid language setting

--- Assets/Scripts/Loader/LanguageSelector.cs
+++ Assets/Scripts/Loader/LanguageSelector.cs
@@ -8,7 +8,7 @@ namespace Sketch.Loader
         public void ChangeLanguage()
         {
             if (Translate.Instance.CurrentLanguage == "english") Translate.Instance.CurrentLanguage = "french";
-            else if (Translate.Instance.CurrentLanguage == "french") Translate.Instance.CurrentLanguage = "japanese";
+            //else if (Translate.Instance.CurrentLanguage == "french") Translate.Instance.CurrentLanguage = "japanese";
             else Translate.Instance.CurrentLanguage = "english";
         }
     }
