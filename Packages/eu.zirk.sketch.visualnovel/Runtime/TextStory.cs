99eaaca50a0b8fbc03d09e07e7ced02fb69ca625
Xwilarg <xwilarg@protonmail.com>
2026-04-22 11:12:40 +0200

    Add text story

---
+++ Packages/eu.zirk.sketch.visualnovel/Runtime/TextStory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sketch.VN
+{
+    public class TextStory : IStory
+    {
+        private string[] _dialogues;
+        private int _index;
+
+        public TextStory(string[] dialogues)
+        {
+            _dialogues = dialogues;
+            _index = 0;
+        }
+
+        public IEnumerable<IChoice> Choices => Array.Empty<IChoice>();
+
+        public IEnumerable<string> CurrentTags => Array.Empty<string>();
+
+        public bool CanContinue => _index < _dialogues.Length;
+
+        public void ChoosePath(IChoice choice)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string Continue()
+        {
+            var text = _dialogues[_index];
+            _index++;
+            return text;
+        }
+    }
+}
